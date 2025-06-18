fefb026bd21bac2a2009e93cafe0ea4bd3dc00a8
TechnicallyWeb3 <mancinoweb3@gmail.com>
2025-06-18 00:54:16 -0600

    fixed deployment data

--- esp.deployments.ts
+++ esp.deployments.ts
@@ -44,14 +44,14 @@ export const espDeployments: ESPDeployments = {
       dps: {
         contractAddress: '0xDA7Adb41ac559e689fE170aE4f2853a450e6E4Cc',
         deployerAddress: '0x02040ee763D6d849515cE194bDa93d12bf3c45d0',
-        txHash: 'TBD',
-        deployedAt: '2025-06-18T06:04:27.350Z'
+        txHash: '0x4b953bc3f64a799306ba4d785c0796d8e9134ad830866285cc7819406f062c6a',
+        deployedAt: '2025-06-18T04:18:00.000Z'
       },
       dpr: {
         contractAddress: '0xDA7Ae59Fa1DB9B138dddaaFB6153B11B162Cfd8B',
         deployerAddress: '0x02040ee763D6d849515cE194bDa93d12bf3c45d0',
-        txHash: 'TBD',
-        deployedAt: '2025-06-18T06:04:27.350Z',
+        txHash: '0x7dde4ce8ba920276fe06c786625d0a83e843c19783102b9fad4145607e8d3a7f',
+        deployedAt: '2025-06-18T04:18:00.000Z',
         constructors: {
           ownerAddress: '0xDA00006427E534B1Acde93B9E66d8A9d2C66B2d3',
           dpsAddress: '0xDA7Adb41ac559e689fE170aE4f2853a450e6E4Cc',
